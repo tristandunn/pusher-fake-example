b0f29a5f4130baf6fe112f6bf0ac121cc46bba8d
Tristan Dunn <tristanzdunn@gmail.com>
2012-02-11 15:10:12 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PusherFakeExample::Application.config.secret_token = '45660c47608c3d9ac60dee96a79dc6087b21e6e23c4e05c9514d69565447ce94b2a6f443bb7e8d2f7a5db6fd702be6cacd7ea5740acae98f14601e71d4638f3f'
