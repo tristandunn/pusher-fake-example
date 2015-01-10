d3e054b08181061ce2c9dba7895189f3a42a47df
Tristan Dunn <tristanzdunn@gmail.com>
2015-01-09 20:31:52 -0600

    Update Rails configuration.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-PusherFakeExample::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -12,7 +12,7 @@ PusherFakeExample::Application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure static asset server for tests with Cache-Control for performance.
+  # Configure static file server for tests with Cache-Control for performance.
   config.serve_static_files   = true
   config.static_cache_control = "public, max-age=3600"
 
@@ -26,6 +26,12 @@ PusherFakeExample::Application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Randomize the order test cases are executed.
+  config.active_support.test_order = :random
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
