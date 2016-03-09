cb6280dafacfa0d6262712926c9797a31d0b2c41
pik <alexander.maznev@gmail.com>
2016-03-09 17:33:06 +0800

    Add PusherFake to initializers for development

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,16 @@
-Pusher.app_id = "PUSHER_APP_ID"
-Pusher.key    = "PUSHER_API_KEY"
-Pusher.secret = "PUSHER_API_SECRET"
+require 'pusher'
+# Ensure it's only run in a development environment if it's a global file.
+if Rails.env.development?
+  # Ensure Pusher configuration is set if you're not doing so elsewhere.
+  Pusher.app_id = "MY_TEST_ID"
+  Pusher.key    = "MY_TEST_KEY"
+  Pusher.secret = "MY_TEST_SECRET"
+
+  # Require the base file, which immediately starts the socket and web servers.
+  require "pusher-fake/support/base"
+
+  PusherFake.configure do |configuration|
+    # configuration.logger = Rails.logger
+    # configuration.verbose = true
+  end
+end
